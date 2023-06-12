da6fa26e92ed5af0be0c523c87774703e7d4ec7e
Sudhakar J <35969068+Sudhakar7J@users.noreply.github.com>
2023-06-12 17:18:43 +0530

    Implemented framer motion carousel

--- components/MainPage/LandingCarousel.tsx
+++ components/MainPage/LandingCarousel.tsx
@@ -1,26 +1,51 @@
 "use client"
 
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Image from "next/image"
 import { MockCarouselData } from "@/mockdataconfigs/MockCarouselData"
+import { motion } from "framer-motion"
 
-import { Separator } from "../ui/separator"
+import { useScreenSize } from "@/hooks/useScreenSize"
+
+import { CarouselItem } from "./CarouselItem"
+import { SponsoredCarouselItem } from "./SponsoredCarouselItem"
 
 export function LandingCarousel() {
-  const [selectedCategory, setSelectedCategory] = useState("impact")
+  const { isMobileScreen } = useScreenSize()
+  const [highlightedArticle, setHighlightedArticle] = useState(1)
 
-  const handleCategoryClick = (category: React.SetStateAction<string>) => {
-    setSelectedCategory(category)
+  const onHighlightArticle = (id: React.SetStateAction<number>) => {
+    if (isMobileScreen) {
+      return
+    }
+    setHighlightedArticle(id)
   }
 
-  const selectedImage = MockCarouselData().find(
-    (carouselItem) => carouselItem.category === selectedCategory
-  )?.image
+  const selectedImage = MockCarouselData.find(
+    (carouselItem) => carouselItem.id === highlightedArticle
+  )?.imageUrl
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHighlightedArticle((prevState) => {
+        const selectedIndex = prevState % MockCarouselData.length
+
+        return MockCarouselData[selectedIndex].id
+      })
+    }, 4000)
+
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div className="min-h-screen min-w-full">
-      <div className="relative flex h-1/3 w-full justify-center md:m-0 md:h-2/3 md:w-full">
-        {selectedImage && (
+      {selectedImage && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="relative flex h-1/3 w-full justify-center md:m-0 md:h-2/3 md:w-full"
+        >
           <Image
             src={selectedImage}
             alt="logo"
@@ -28,37 +53,35 @@ export function LandingCarousel() {
             style={{ objectFit: "cover" }}
             className="contain"
           />
-        )}
-      </div>
+        </motion.div>
+      )}
       <div className="flex cursor-pointer flex-col md:grid md:grid-cols-4 md:gap-4">
-        {MockCarouselData().map((carouselItem, index) => (
-          <React.Fragment key={carouselItem.title}>
-            <div
-              className="flex bg-white p-4  md:border-gray-500 md:opacity-30 md:hover:border-t-8 md:hover:bg-gray-100 md:hover:opacity-100"
-              onClick={() => handleCategoryClick(carouselItem.category)}
-            >
-              <div className="flex w-2/3 grow flex-col md:w-max">
-                <div className="font-bold uppercase text-gray-700">
-                  {carouselItem.category}
-                </div>
-                <div className="flex text-lg font-bold leading-5 md:text-2xl md:leading-none">
-                  {carouselItem.title}
-                </div>
-              </div>
-              <div className="relative my-auto w-1/3">
-                <div className=" relative items-center justify-center md:hidden ">
-                  <Image
-                    src={carouselItem.image}
-                    alt={carouselItem.title}
-                    width={1000}
-                    height={200}
-                  />
-                </div>
-              </div>
-            </div>
-            <Separator className="md:hidden" />
-          </React.Fragment>
-        ))}
+        {MockCarouselData.map(
+          ({ title, id, imageUrl, category, isSponsoredArticle }) =>
+            isSponsoredArticle ? (
+              <SponsoredCarouselItem
+                key={id}
+                {...{
+                  id,
+                  title,
+                  category,
+                  imageUrl,
+                }}
+              />
+            ) : (
+              <CarouselItem
+                key={id}
+                {...{
+                  id,
+                  title,
+                  category,
+                  imageUrl,
+                  onHighlightArticle,
+                  highlightedArticle,
+                }}
+              />
+            )
+        )}
       </div>
     </div>
   )
